9d3b7a54fb857f4874d6cae2af98bd1e6f95e064
maxmaxph <maxmaxph@gmail.com>
2023-09-14 10:06:27 +0200

    0 accessibilité modal + disclosure + overlay + balise alt + bonus

--- js/disclosure.js
+++ js/disclosure.js
@@ -2,7 +2,26 @@ let discButton = document.getElementById("useDsicl");
 let disclosure = document.getElementById("disclosureWindow");
 
 document.addEventListener("DOMContentLoaded", () => {
+  // visibilité de ma disclosure //
   discButton.addEventListener("click", (e) => {
-    disclosure.style.display = "block";
+    if (disclosure.style.display === "block") {
+      disclosure.style.display = "none";
+    } else {
+      disclosure.style.display = "block";
+    }
+  });
+
+  // Fermer la disclosure avec un clic en dehors du disclosure //
+  document.addEventListener("click", (e) => {
+    if (e.target !== discButton && !disclosure.contains(e.target)) {
+      disclosure.style.display = "none";
+    }
+  });
+
+  // Fermer la disclosure avec la touche Échap (bonus) //
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      disclosure.style.display = "none";
+    }
   });
 });
--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,53 @@
-let modal = document.getElementById("modalWindow");
-let openButton = document.getElementById("openMod");
+const modal = document.getElementById("modalWindow");
+const openButton = document.getElementById("openMod");
+const closeButton = modal.querySelector("button"); //je selectionne egalement le closeButton //
+const overlay = document.getElementById("overlay"); //fond grisé à l'ouverture de la modale //
 
 document.addEventListener("DOMContentLoaded", () => {
-  var focusable = modal.querySelectorAll(
+
+  const focusable = modal.querySelectorAll(
     'button, [href], [tabindex]:not([tabindex="-1"])'
   );
-  var firstFocusable = focusable[0];
-  var lastFocusable = focusable[focusable.length - 1];
-  console.log(focusable);
+  const [firstFocusable, ...rest] = Array.from(focusable);
+  const lastFocusable = rest.pop();
+
+  // je modifie l'evenement pour ouvrir la modale //
   openButton.addEventListener("click", (event) => {
     event.preventDefault();
     modal.style.display = "flex";
+    modal.setAttribute("aria-hidden", "false");
+    firstFocusable.focus(); // Mettre le focus sur le premier élément focusable //
+    overlay.style.display = "block"; // Afficher le fond grisé (overlay) //
   });
 
-  firstFocusable.addEventListener("click", (event) => {
+  // j'ai préféré créer un  evenement pour fermer la modale également //
+  closeButton.addEventListener("click", (event) => {
     event.preventDefault();
     modal.style.display = "none";
-    openButton.focus();
+    modal.setAttribute("aria-hidden", "true");
+    openButton.focus(); // Retourner le focus au bouton d'ouverture //
+    overlay.style.display = "none"; // Cacher le fond grisé (overlay) //
+  });
+
+  // je commande avec mon clavier quand ma modale est open //
+  document.addEventListener("keydown", (event) => {
+    // Je ferme la modal avec la touche ECHAP! //
+    if (
+      event.key === "Escape" &&
+      modal.getAttribute("aria-hidden") === "false"
+    ) {
+      closeButton.click();
+    }
+
+    // je "piège" le focus à l'intérieur de la modale //
+    if (event.key === "Tab" && modal.getAttribute("aria-hidden") === "false") {
+      if (event.shiftKey && document.activeElement === firstFocusable) {
+        event.preventDefault();
+        lastFocusable.focus(); // Si on est sur le premier élément et qu'on appuie sur Shift+Tab, ca va au dernier élément //
+      } else if (!event.shiftKey && document.activeElement === lastFocusable) {
+        event.preventDefault();
+        firstFocusable.focus(); // Si on est sur le dernier élément et qu'on appuie sur Tab, retourner au premier élément //
+      }
+    }
   });
 });
